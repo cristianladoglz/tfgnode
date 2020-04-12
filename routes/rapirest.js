281171a49c5b800ac3f970dd01352af9434f04ff
cristianladoglz <cris.1998@hotmail.es>
2020-04-12 10:52:05 +0200

    API: Añadir eventos de infracciones diferentes a faltas personales

--- routes/rapirest.js
+++ routes/rapirest.js
@@ -73,6 +73,30 @@ module.exports = function(app, swig, DBManager, io) {
     });
 
     /**
+     * Return a list with team coaches
+     */
+    app.get("/api/coaches/:teamName", function(req, res) {
+        var criterion = {
+            teamName: req.params.teamName
+        };
+
+        DBManager.getCoaches(criterion, function (coaches) {
+            if (coaches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send("{"+
+                    '"coaches":'+
+                    JSON.stringify(coaches)+
+                    "}");
+            }
+        });
+    });
+
+    /**
      * Start a match
      */
     app.put("/api/start/match/:id", function(req, res) {
@@ -375,6 +399,385 @@ module.exports = function(app, swig, DBManager, io) {
                     eventType: "timeout"
                 };
                 DBManager.insertEvent(timeout, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event overAndBack
+     */
+    app.post("/api/add/overAndBack", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var overAndBack = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "overAndBack"
+                };
+                DBManager.insertEvent(overAndBack, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event shotClock
+     */
+    app.post("/api/add/shotClock", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var shotClock = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "shotClock"
+                };
+                DBManager.insertEvent(shotClock, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event eightSeconds
+     */
+    app.post("/api/add/eightSeconds", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var eightSeconds = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "eightSeconds"
+                };
+                DBManager.insertEvent(eightSeconds, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event travelling
+     */
+    app.post("/api/add/travelling", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var travelling = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "travelling"
+                };
+                DBManager.insertEvent(travelling, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event doouble
+     */
+    app.post("/api/add/double", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var double = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "double"
+                };
+                DBManager.insertEvent(double, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event foot
+     */
+    app.post("/api/add/foot", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var foot = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "foot"
+                };
+                DBManager.insertEvent(foot, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event fiveSeconds
+     */
+    app.post("/api/add/fiveSeconds", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var fiveSeconds = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "fiveSeconds"
+                };
+                DBManager.insertEvent(fiveSeconds, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event threeSeconds
+     */
+    app.post("/api/add/threeSeconds", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var threeSeconds = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "threeSeconds"
+                };
+                DBManager.insertEvent(threeSeconds, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event unsportmanlike
+     */
+    app.post("/api/add/unsportmanlike", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var unsportmanlike = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "unsportmanlike"
+                };
+                DBManager.insertEvent(unsportmanlike, function (newEvent) {
+                    if (newEvent == null) {
+                        res.status(500);
+                        res.json({
+                            error: "Se ha producido un error"
+                        })
+                    } else {
+                        res.status(201);
+                        res.send(JSON.stringify(newEvent));
+                    }
+
+                });
+            }
+        });
+    });
+
+    /**
+     * Add new event technicalFoul
+     */
+    app.post("/api/add/technicalFoul", function(req, res) {
+        DBManager.getMatches({ "_id" : DBManager.mongo.ObjectID(req.body.matchId)},function(matches) {
+            if (matches == null) {
+                res.status(500);
+                res.json({
+                    error: "Se ha producido un error"
+                })
+            } else {
+                var idPlayer = "";
+                if(req.body.playerId!=="")
+                    idPlayer = DBManager.mongo.ObjectID(req.body.playerId);
+
+                var technicalFoul = {
+                    matchId: DBManager.mongo.ObjectID(req.body.matchId),
+                    time: req.body.time,
+                    playerId: idPlayer,
+                    playerName: req.body.playerName,
+                    playerBib: req.body.playerBib,
+                    teamId: DBManager.mongo.ObjectID(req.body.teamId),
+                    eventType: "technicalFoul"
+                };
+                DBManager.insertEvent(technicalFoul, function (newEvent) {
                     if (newEvent == null) {
                         res.status(500);
                         res.json({
