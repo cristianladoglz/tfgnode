d7bc342c6fcae6f7aad9b454bcc8385ca93d6974
cristianladoglz <cris.1998@hotmail.es>
2020-04-28 11:53:04 +0200

    Modificar partido en estado creado

--- routes/rmatches.js
+++ routes/rmatches.js
@@ -89,7 +89,6 @@ module.exports = function(app, swig, DBManager, validationManager) {
         var criterion={_id:matchId};
 
         DBManager.getMatches(criterion, function(matches){
-
             var matchCriterion = {"_id": matchId};
             var match = {
                 state: "active"
@@ -101,6 +100,81 @@ module.exports = function(app, swig, DBManager, validationManager) {
                     res.redirect("/match/mine");
                 }
             });
+        });
+    });
+
+    app.get('/match/modify/:id', function (req, res) {
+        var matchId = DBManager.mongo.ObjectID(req.params.id);
+        var user=req.session.usuario;
+        var criterion={_id:matchId};
+
+        DBManager.getMatches(criterion, function(matches){
+            if(matches.length === 0)
+                res.send("Error al obtener partido");
+
+            var criterion={};
+
+            DBManager.getTeams(criterion, function(teams){
+                if (teams == null) {
+                    res.send("Error al obtener equipos");
+                } else {
+                    var respuesta = showView('views/modifyMatch.html', {match: matches[0], teams: teams}, req.session);
+                    res.send(respuesta);
+                }
+            });
+        });
+    });
+
+    app.post("/match/:id", function(req, res) {
+        var validateMatch = {
+            localTeam : req.body.local.valueOf(),
+            visitorTeam : req.body.visitor.valueOf(),
+            matchCourt : req.body.courtSelect.valueOf(),
+            runningTime : req.body.runningTime.valueOf()
+        };
+
+        validationManager.matchCreation(validateMatch,function(message){
+            if(message!=null) {
+                res.redirect("/match/add" +
+                    "?message=" + message +
+                    "&messageType=alert-danger");
+            }else {
+
+                var tableOfficial="";
+                var court=req.body.courtSelect.valueOf();
+
+                if(req.body.tableOfficial.valueOf()!=="none")
+                    tableOfficial=req.body.tableOfficial.valueOf().toString();
+
+                if(court==="otro")
+                    court=req.body.court;
+
+                var runningTime = req.body.runningTime.valueOf() === "true";
+
+                var match = {
+                    localTeam : req.body.local.valueOf(),
+                    visitorTeam : req.body.visitor.valueOf(),
+                    quartersNumber : parseInt(req.body.quarter),
+                    durationQuarter : parseInt(req.body.duration),
+                    runningTime : runningTime,
+                    timeOuts : parseInt(req.body.timeOut),
+                    maxPersonalFouls : parseInt(req.body.personalFoul),
+                    date : req.body.date,
+                    time : req.body.time,
+                    matchCourt : court,
+                    tableOfficial : tableOfficial
+                };
+
+                var matchId = DBManager.mongo.ObjectID(req.params.id);
+                var matchCriterion = {"_id": matchId};
+                DBManager.modifyMatch(matchCriterion, match, function (id) {
+                    if (id == null) {
+                        res.redirect("/match/modify/"+req.params.id+"?message=Error al modificar partido");
+                    } else {
+                        res.redirect("/match/mine?message=Partido modificado correctamente");
+                    }
+                });
+            }
         });
     });
 
